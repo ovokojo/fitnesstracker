5b88003b69293756469eb0f8ac2b52e7ef95363b
Kojo Addaquay <kojoaddaquay@gmail.com>
2020-09-08 02:37:37 +0000

    Abstracted Firebase auth, modified gitignore

---
+++ lib/services/auth.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class User {
+  User({@required this.uid});
+  final String uid;
+}
+
+abstract class AuthBase {
+  Future<User> currentUser();
+  Future<User> loginAnonymously();
+  Future<void> logout();
+}
+
+class Auth implements AuthBase {
+  final _auth = FirebaseAuth.instance;
+
+  User _userFromFirebase(FirebaseUser user) {
+    return user == null ? null : User(uid: user.uid);
+  }
+  @override
+  Future<User> currentUser() async {
+    final currentUser = await _auth.currentUser();
+    return _userFromFirebase(currentUser);
+  }
+  @override
+  Future<User> loginAnonymously() async {
+    final authResult = await _auth.signInAnonymously();
+    return _userFromFirebase(authResult.user);
+  }
+  @override
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
