0040c5806394e0a10d5c465c48f695a1991680a5
Kojo Addaquay <kojoaddaquay@gmail.com>
2020-10-13 02:19:18 +0000

    Toggle signup/login

--- lib/app/login/email_form.dart
+++ lib/app/login/email_form.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:timetracker/common_widgets/form_submit_button.dart';
 
-class EmailLoginForm extends StatelessWidget {
+enum EmailFormType { Login, Register }
+
+class EmailLoginForm extends StatefulWidget {
+  @override
+  _EmailLoginFormState createState() => _EmailLoginFormState();
+}
+
+class _EmailLoginFormState extends State<EmailLoginForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  var formType = EmailFormType.Login;
+
+  void _submitForm() {
+    print(
+        'email: ${_emailController.text} | pass: ${_passwordController.text}');
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      formType == EmailFormType.Login
+          ? formType = EmailFormType.Register
+          : formType = EmailFormType.Login;
+    });
+  }
+
   List<Widget> _buildChildren() {
+    final _primaryText =
+        formType == EmailFormType.Login ? 'Login' : 'Create Account';
+    final _secondaryText = formType == EmailFormType.Login
+        ? 'Need an Account? Register'
+        : 'Have an Account? Login';
+
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'user@email.com',
         ),
       ),
       TextField(
+        controller: _passwordController,
         obscureText: true,
         decoration: InputDecoration(
           labelText: 'Password',
@@ -19,8 +51,8 @@ class EmailLoginForm extends StatelessWidget {
       SizedBox(
         height: 20,
       ),
-      FormSubmitButton(text: 'Login', onPressed: (){}),
-      FlatButton(onPressed: () {}, child: Text('Need an Account? Register'))
+      FormSubmitButton(text: _primaryText, onPressed: _submitForm),
+      FlatButton(onPressed: _toggleFormType, child: Text(_secondaryText))
     ];
   }
 
